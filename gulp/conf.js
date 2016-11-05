9195a5002f842de784ca12535928538c2334d01e
Pascoal Gomes <pascoal@mfdc.biz>
2016-11-05 11:59:21 +1000

    initialise the app structure with necessary tools

---
+++ gulp/conf.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * The main paths of this project.
+ * Please handle with care!
+ */
+exports.path = {
+	root:'./',
+	build: './.build',
+	tmp: './.tmp',
+	app: './app'
+};
+
+exports.sources = {
+			
+};
